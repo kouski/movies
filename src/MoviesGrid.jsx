4bfa0a8807a185ea9af383fa13d5f5a228e14b02
kouski <72550619+kouski@users.noreply.github.com>
2023-10-06 23:31:00 +0200

    Configuracion inicial

---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,17 @@
+import MovieCard from './MovieCard';
+import movies from './movies.json';
+import styles from './MoviesGrid.module.css';
+
+const MoviesGrid = () => {
+  return (
+    
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+    
+  )
+}
+
+export default MoviesGrid
